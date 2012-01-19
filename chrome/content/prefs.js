194b501be8a8479e04bfa73c36897e397c626aeb
Kevin Cox <kevincox.ca@gmail.com>
2012-01-18 22:30:41 -0500

    Fixed global overwrite.

--- chrome/content/prefs.js
+++ chrome/content/prefs.js
@@ -4,15 +4,15 @@ var autosizerPref = {
 	prefs: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch),
 	init: function() {
 		try { if(window.arguments[0]) window.opener=window.arguments[0]; } catch(e){}
-		for(p in this.intP) document.getElementById(this.intP[p]).value=this.prefs.getIntPref('extensions.autosizer.'+this.intP[p]);
-		for(p in this.boolP) document.getElementById(this.boolP[p]).checked=this.prefs.getBoolPref('extensions.autosizer.'+this.boolP[p]);
+		for(var p in this.intP) document.getElementById(this.intP[p]).value=this.prefs.getIntPref('extensions.autosizer.'+this.intP[p]);
+		for(var p in this.boolP) document.getElementById(this.boolP[p]).checked=this.prefs.getBoolPref('extensions.autosizer.'+this.boolP[p]);
 
 		this.setEnDis();
 	},
 
 	save: function() {
-		for(p in this.intP) this.prefs.setIntPref('extensions.autosizer.'+this.intP[p],document.getElementById(this.intP[p]).value);
-		for(p in this.boolP) this.prefs.setBoolPref('extensions.autosizer.'+this.boolP[p],document.getElementById(this.boolP[p]).checked);
+		for(var p in this.intP) this.prefs.setIntPref('extensions.autosizer.'+this.intP[p],document.getElementById(this.intP[p]).value);
+		for(var p in this.boolP) this.prefs.setBoolPref('extensions.autosizer.'+this.boolP[p],document.getElementById(this.boolP[p]).checked);
 
 		/*** Update the searchbars acording to the new prefrence. ***/
 		var wi = Components.classes["@mozilla.org/appshell/window-mediator;1"]
